db2760aa77e388afdda9e1c72bda08fbe870f52f
Gagandeep Sandhu <sandhugagan400@gmail.com>
2019-09-03 13:39:57 +1200

    unit test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RUSSIANROULETTE_GAME;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        GameLogic unitTestObj = new GameLogic();
+        [TestMethod]
+        public void TestShootaway()
+        {
+            var nav = unitTestObj.Shootaway(2);
+            Assert.AreEqual(expected: 2, nav);
+        }
+
+        [TestMethod]
+        public void TestShootawayPositive()
+        {
+            var nav = unitTestObj.Shootaway(0);
+            Assert.AreEqual(expected: 2, nav);
+        }
+    }
+}
